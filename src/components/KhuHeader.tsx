d10298642e164e9cf5160ecb5c6fd0c11d369a64
seongho5356 <kjeng7897@gmail.com>
2025-04-04 15:06:26 +0900

    refactor: 상단바 색상 수정

--- src/components/KhuHeader.tsx
+++ src/components/KhuHeader.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const KhuHeader: React.FC = () => {
   return (
-    <div className="bg-khu-green text-white p-4 rounded-t-lg shadow-md">
+    <div className="bg-blue-500 text-white p-4 rounded-t-lg shadow-md">
       <h1 className="text-xl font-bold text-center">경희대학교</h1>
       <p className="text-sm text-center mt-1 text-khu-lightgray">강의실 예약 정보</p>
     </div>
